539761b6e07f19aa1ac82786803dc0952883e8e1
bodiem13 <30871690+bodiem13@users.noreply.github.com>
2020-02-29 22:20:44 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,22 +9,22 @@ namespace CSV_File_Reader
             string sourceFile = "/Users/mikebodie/SoftwareDevelopment/MikeSampleData/superbowl.csv";
             string targetFile = "/Users/mikebodie/SoftwareDevelopment/MikeSampleData/OutputData.csv";
             //Test 1
-            FileFilter fileter = new FileFilter(sourceFile);
+            FileFilter fileter = new FileFilter(sourceFile, true);
             //fileter.Filter("Qa", 2);
             //fileter.SortAscend(1);
             //fileter.Save(targetFile);
 
-            //Test 2
-            //fileter = new FileFilter(sourceFile);
-            ////fileter.HasHeader = true;
-            //fileter.Filter("Patriots", 2);
-            //fileter.SortAscend(3);
-            //fileter.Save(targetFile);
-
-            //Test 3
-            fileter.Filter("florida", "State");
-            fileter.SortAscend("Date");
+            ////Test 2
+            fileter = new FileFilter(sourceFile, true);
+            //fileter.HasHeader = true;
+            fileter.Filter("Patriots", 2);
+            fileter.SortAscend(3);
             fileter.Save(targetFile);
+
+            ////Test 3
+            //fileter.Filter("florida", "State");
+            //fileter.SortAscend("Date");
+            //fileter.Save(targetFile);
         }
     }
 }
